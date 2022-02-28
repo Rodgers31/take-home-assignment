165f89a79ffa6e2f9acdea2cb2f44f38a8657c3d
Rodgers31 <otienor986@gmail.com>
2022-02-28 14:27:18 -0500

    setup express and env

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const server = require('express')();
+const express = require('express');
+const cors = require('cors');
+const server = express();
 
-server.listen(500, () => {
-  console.log('running on port 5000');
+server.use(express.json());
+server.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+server.get('/', (req, res) => {
+  res.send(`<h1>Welcome to my project<h1/>`);
+});
+
+server.get('/api', (req, res) => {
+  res.json({ message: 'My prject rocks' });
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
