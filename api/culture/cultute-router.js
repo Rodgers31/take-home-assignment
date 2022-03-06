ac66a7c92e69d225a5dc7d63cbb98e4e071e3ab2
Rodgers31 <otienor986@gmail.com>
2022-03-06 00:52:10 -0500

    setup debugger and added id router

--- api/culture/cultute-router.js
+++ api/culture/cultute-router.js
@@ -11,16 +11,31 @@ router.get('/', async (req, res, next) => {
     })
     .catch(next);
 });
+router.get('/:id', async (req, res, next) => {
+  Culture.getById(req.params.id)
+    .then((culture) => {
+      if (!culture) {
+        res.status(404).json({
+          message: 'The culture does not exist',
+        });
+      } else {
+        res.status(200).json(culture);
+      }
+    })
+    .catch(next);
+});
 router.put('/:id', async (req, res, next) => {
   const id = req.params.id;
   const changes = req.body;
   Culture.update(id, changes)
     .then((change) => {
-      if (change === true) {
-        Culture.finById(id).then((item) => {
-          res
-            .status(200)
-            .json({ message: `${item.url} has been changes`, item });
+      if (changes.forming == true) {
+        Culture.getById(id).then((item) => {
+          res.status(200).json({ message: `item is forming` });
+        });
+      } else if (changes.forming == false) {
+        Culture.getById(id).then((item) => {
+          res.status(200).json({ message: `item not forming` });
         });
       } else {
         next();
