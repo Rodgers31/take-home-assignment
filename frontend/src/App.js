c6bad196bf629d1afcffd62a540baa785f0eef70
Rodgers31 <otienor986@gmail.com>
2022-03-06 22:30:43 -0500

    started working on frotend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Culture from './components/Culture';
 
 const App = () => {
   return (
     <div>
-      <h1>My react app</h1>
+      <Culture />
     </div>
   );
 };
